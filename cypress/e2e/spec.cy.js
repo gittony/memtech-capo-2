f5c57feea0b0191c387dd4211b6de74b7c5539db
Tony White <tonywhite@gmail.com>
2022-08-12 10:36:53 -0500

    adding paragraph changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,6 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/')
     cy.get('h1').should('contain.text', 'hello').should('be.visible')
+    cy.get('p').should('contain.text', 'i am fine').should('be.visible')
   })
 })
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
     <h1>hello</h1>
-    <p>how are you</p>
+    <p>how are you, i am fine, you are too</p>
     </div>
   )
 }
